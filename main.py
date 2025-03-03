903250917bd625e9ea97e2c57783a6727019fe48
ryanc <Ryan_Copeland@student.uml.edu>
2025-03-03 15:23:11 -0500

    Refactoring phase 1 for later sanity

---
+++ main.py
@@ -0,0 +1,4 @@
+import phase_1
+
+if __name__ == "__main__":
+    Translator = phase_1.Translator()
--- phase_1.py
+++ phase_1.py
@@ -1,4 +1,36 @@
-chart : dict = {
+class Translator():
+    def __init__(self):
+        self.hash_dec()
+        self.translate("Testing")
+
+
+    def cli(self) -> None:
+        pass
+
+    def translate(self, instr : str) -> None:
+        outstr : str = ""
+
+        instr = instr.lower()
+        i : int = 0
+
+        while i < len(instr):
+            if (instr[i] == "t") and (instr[i + 1] == "h"):
+                outstr = outstr + self.chart["th"]
+                i = i + 1
+
+            elif (instr[i] == "n") and (instr[i + 1] == "g"):
+                outstr = outstr + self.chart["ng"]
+                i = i + 1
+
+            else:
+                outstr = outstr + self.chart[instr[i]]
+
+            i = i + 1
+
+        print(outstr)
+
+    def hash_dec(self) -> None: #completely unneeded, I just wanted to put the dict dec out of the way.
+        self.chart :dict = {
     "a" : "ᚪ",
     "b" : "ᛒ",
     "c" : "ᚳ",
@@ -29,25 +61,3 @@ chart : dict = {
     "ng": "ᛝ",
     " " : " "
 }
-
-instr : str = "running"
-outstr : str = ""
-
-instr = instr.lower()
-i : int = 0
-
-while i < len(instr):
-    if (instr[i] == "t") and (instr[i + 1] == "h"):
-        outstr = outstr + chart["th"]
-        i = i + 1
-
-    elif (instr[i] == "n") and (instr[i + 1] == "g"):
-        outstr = outstr + chart["ng"]
-        i = i + 1
-
-    else:
-        outstr = outstr + chart[instr[i]]
-
-    i = i + 1
-
-print(outstr)
