ce65967af4846982fc88e624d22a12c8e0211555
Ryan T Copeland <123291263+RyanC528@users.noreply.github.com>
2025-03-04 16:52:14 -0500

    Delete phase_2.py

--- phase_2.py
+++
@@ -1,9 +0,0 @@
-def file_bs() -> None:
-    f_in = open("/home/ryanc/futhorcify/text_files/input.txt","rt")
-    f_out = open("/home/ryanc/futhorcify/text_files/output.txt","wt")
-
-    for line in f_in:
-        f_out.write(line)
-
-    f_in.close()
-    f_out.close()
