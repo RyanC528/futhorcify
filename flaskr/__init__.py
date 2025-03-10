d08f9abc5a9c55f02b6641a49a75b112b08eab66
Ryan_Copeland@student.uml.edu <ryanc@DESKTOP-ELG60CG>
2025-03-09 21:52:50 -0400

    added base flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,27 @@
 import os
 
 from flask import Flask
+
+
+# source https://flask.palletsprojects.com/en/stable/tutorial/factory/
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",  # CHANGE LATTER
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello,world!"
+
+    return app
