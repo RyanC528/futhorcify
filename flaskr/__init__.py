a062546127a78446b02acd8f38ffd6c2eb66ff04
ryanc <ryan.copeland528@gmail.com>
2025-03-26 23:28:39 -0400

    I SPELT CONTENT CONTEND

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,37 +1,40 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flaskr.translator import Translator
 
 translation = "ᚻᚩᚣᛞ"
 trans = Translator()
 
 
-# source https://flask.palletsprojects.com/en/stable/tutorial/factory/
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    translation = trans.get_outstr()
-    app.config.from_mapping(
-        SECRET_KEY="dev",  # CHANGE LATTER
-    )
+app = Flask(__name__)
 
-    if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+@app.route("/home")
+def home():
+    pass
 
-    @app.route("/", methods=["GET", "POST"])
-    def base():
-        translation = "ᚻᚩᚣᛞ"
-        trans = Translator()
-        if request.method == "POST":
-            trans.set_instr(request.form["input"])
-            translation = trans.get_outstr()
-        return render_template("index.html", translation=translation)
 
-    return app
+@app.route("/tranlator", methods=["GET", "POST"])
+def translator():
+    translation = "ᚻᚩᚣᛞ"
+    trans = Translator()
+    if request.method == "POST":
+        trans.set_instr(request.form["input"])
+        translation = trans.get_outstr()
+        return redirect(url_for(staticmethod))
+    return render_template("trans.html", translation=translation)
+
+
+@app.route("/explanation")
+def explanation():
+    pass
+
+
+@app.route("/key")
+def key():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
